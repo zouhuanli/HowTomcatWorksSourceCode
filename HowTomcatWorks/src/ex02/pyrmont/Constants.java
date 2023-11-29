0467916cdc3c61f5c4cf3f27983573152158e7c0
zouhl <zouhl0924@gmail.com>
2023-11-29 21:17:13 +0800

    第二章代码

--- HowTomcatWorks/src/ex02/pyrmont/Constants.java
+++ HowTomcatWorks/src/ex02/pyrmont/Constants.java
@@ -3,6 +3,7 @@ package ex02.pyrmont;
 import java.io.File;
 
 public class Constants {
+    // TODO: 2023/11/29 不要使用绝对路径 
     public static final String WEB_ROOT =
             "D:\\git\\code\\HowTomcatWorksSourceCode\\HowTomcatWorks" + File.separator + "webroot";
 }
